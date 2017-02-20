f5a610a18a926122027f7fc20e9cbf55ef17b58a
almusto <ales.musto@gmail.com>
2017-02-20 13:30:16 -0500

    row row row

--- Protocols/BankVault.swift
+++ Protocols/BankVault.swift
@@ -6,6 +6,31 @@
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
 
+protocol ProvideAccess {
+  func allowEntryWithPassword(_ password: [Int]) -> Bool
+}
+
+extension BankVault : ProvideAccess {
+  func allowEntryWithPassword(_ password: [Int]) -> Bool {
+    if password.isEmpty || password.count > 10 {
+      return false
+    } else if everyOtherEven(password) == true {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  func everyOtherEven(_ password: [Int]) -> Bool {
+    for i in stride(from: 0, to: password.count - 1, by: 2) {
+      if password[i] % 2 != 0 {
+        return false
+      }
+    }
+    return true
+  }
+
+}
 
 class BankVault {
     
